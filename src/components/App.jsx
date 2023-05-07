c560ab3187cd7f20be1e8adcabb295858aac5c6f
illya15 <113913094+illya15@users.noreply.github.com>
2023-05-07 20:11:32 +0300

    hw2 task2 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,50 @@ import { Component } from 'react';
       name: '',
     };
   
-
+    handleInputChange = event =>{
+this.setState({name: event.currentTarget.value})
+    }
+    handleSubmit = event => { 
+      event.preventDefault()
+      console.log(this.state)
+    }
 
   render() 
   {
     return (
-      <div>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
+      >
+        <form onSubmit={this.handleSubmit}
+          
+        >
+          <label>
+           
+            Name
+            <input
+              type="text"
+              name={this.state.name} onChange={this.handleInputChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type='submit'>Add contacts</button>
+        </form>
+         <div>
+        <h2>Contacts</h2>
+        <ul>
+          <li></li>
+        </ul>
+        </div>
       </div>
     );
   }
