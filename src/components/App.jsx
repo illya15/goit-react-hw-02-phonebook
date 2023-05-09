699d69e8dee29e4f8c592cb8ed96e08f5850e930
illya15 <113913094+illya15@users.noreply.github.com>
2023-05-09 14:02:59 +0300

    hw2 25

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,60 @@
-  // import { render } from '@testing-library/react';
-
-  // import { nanoid } from 'nanoid';
 import { Component } from 'react';
-  
-  export class App extends Component {
-    state = {
-      contacts: [],
-      name: '',
-    };
-  
-    handleInputChange = event =>{
-this.setState({name: event.currentTarget.value})
-    }
-    handleSubmit = event => { 
-      event.preventDefault()
-      console.log(this.state)
-    }
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
 
-  render() 
-  {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <form onSubmit={this.handleSubmit}
-          
-        >
-          <label>
-           
-            Name
-            <input
-              type="text"
-              name={this.state.name} onChange={this.handleInputChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <button type='submit'>Add contacts</button>
-        </form>
-         <div>
-        <h2>Contacts</h2>
-        <ul>
-          <li></li>
-        </ul>
-        </div>
-      </div>
-    );
-  }
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
+
+    addContact = (name, number) => {
+      const duplicate = this.state.contacts.find(
+        contact => contact.name === name
+      );
+      if (duplicate) {
+        alert('Error!');
+        return;
+      }
+      this.setState({
+        contacts: [...this.state.contacts, { id: nanoid(), name, number }],
+      });
+    };
+
+    changeFilter = e => {
+      this.setState({
+        filter: e.currentTarget.value,
+      });
+    };
+
+    deleteContact = id => {
+      const filterId = this.state.contacts.filter(contact => contact.id !== id);
+      this.setState({
+        contacts: [...filterId],
+      });
+    };
+
+    render() {
+      return (
+        <div>
+          <h1>Phonebook</h1>
+          <Form onSubmit={this.addContact} />
+          <h2>Contacts</h2>
+          <Filter onChange={this.changeFilter} value={this.state.filter} />
+          <ContactList
+            onClick={this.deleteContact}
+            contacts={this.state.contacts}
+            filter={this.state.filter}
+          />
+        </div>
+      );
+    }
+  }
+
